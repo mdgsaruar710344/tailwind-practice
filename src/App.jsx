1537638da8f8f1df4d4d9d7d1fce5087dac8689e
mdgsaruar710344 <golamsaruar004@gmail.com>
2024-11-01 01:41:13 +0600

    initialize tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState()
+
+  return (
+    <>
+      <h1 className='text-7xl bg-green-300'>Vite  React</h1>
+    </>
+  )
+}
+
+export default App
